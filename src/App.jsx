b4a1b34e387dea94ceeb74475481993514e3eafb
Dardee Jay C. Calura <68296049+dardeejay@users.noreply.github.com>
2023-02-25 00:07:45 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./styles/styles.css";
 import CurrentWeather from "./components/current-weather";
-import Search from "./components/search";
 function App() {
   return (
     <div className="App">
